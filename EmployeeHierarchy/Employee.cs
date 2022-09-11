706115789175c2b5e70005b89e5da79e04e1452e
Stephen Odundo <steveremdi@gmail.com>
2022-09-11 18:19:54 +0300

    added model for single employee

---
+++ EmployeeHierarchy/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeHierarchy
+{
+    // Represents a single employee
+    internal class Employee
+    {
+        public string ID { get; set; }
+        public string ManagerID { get; set; }
+        public int Salary { get; set; }
+    }
+}
