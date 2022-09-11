b59f66053cea02edb2dd5a26a33a380e978b1ea4
Stephen Odundo <steveremdi@gmail.com>
2022-09-12 01:51:54 +0300

    simple use of extensions library

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Exercise02
+﻿using Exercise01;
+using System.Numerics;
+
+namespace Exercise02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string numberAsWords;
+            Console.WriteLine("Enter any number:");
+            string userValue = Console.ReadLine() ?? "";
+
+            // mild processing to improve conversion success
+            userValue = userValue.Replace(",", "");
+            userValue = userValue.Replace(" ", "");
+
+            if (BigInteger.TryParse(userValue, out BigInteger userBigIntegerValue))
+                numberAsWords = userBigIntegerValue.Towards();
+            else // for outliers
+                numberAsWords = "Could not detect number in input.";
+
+            // Display result
+            Console.WriteLine(numberAsWords);
+            Console.ReadKey();
         }
     }
 }
