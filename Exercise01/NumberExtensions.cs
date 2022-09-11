d102516c99be3398a2f1bb5e68e2fa3a428146d9
Stephen Odundo <steveremdi@gmail.com>
2022-09-11 18:56:49 +0300

    added project for extensions question

---
+++ Exercise01/NumberExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise01
+{
+    public static class NumberExtensions
+    {
+        public static string Towards(this int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
